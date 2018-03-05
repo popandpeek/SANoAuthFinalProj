f4aa4a170f916af5ab63520e9b690ed9d5252dd1
Benjamin D. Pittman <benpitts@uw.edu>
2018-03-05 07:13:27 -0800

    Merge

---
+++ SANoAuthFinalProj/Models/SeedData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SANoAuthFinalProj.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new SAAppContext(
+                serviceProvider.GetRequiredService<DbContextOptions<SAAppContext>>()))
+            {
+                // Look for any movies.
+                if (context.DataPoint.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Sensor.AddRange(
+                    new Sensor
+                    {
+                        Name = "FitBit",
+                        DataPoints = new List<DataPoint>()
+                    },
+                    new Sensor
+                    {
+                        Name = "PillBottle",
+                        DataPoints = new List<DataPoint>()
+                    },
+                    new Sensor
+                    {
+                        Name = "WeightSensor",
+                        DataPoints = new List<DataPoint>()
+                    },
+                    new Sensor
+                    {
+                        Name = "DoorSensor",
+                        DataPoints = new List<DataPoint>()
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
+
--- SANoAuthFinalProj/Program.cs
+++ SANoAuthFinalProj/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SANoAuthFinalProj.Models;
 
 namespace SANoAuthFinalProj
 {
@@ -14,7 +17,28 @@ namespace SANoAuthFinalProj
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<SAAppContext>();
+                    // requires using Microsoft.EntityFrameworkCore;
+                    context.Database.Migrate();
+                    // Requires using RazorPagesMovie.Models;
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
